c63e88144cdd08f91e4bf907973f0eb165c6cc0f
vamsi9885-tech <nukalavamsikrishna06@gmail.com>
2025-04-27 11:14:08 +0530

    Add files via upload

---
+++ apachebeam/CsvBigtable (1).java
@@ -0,0 +1,176 @@
+package com.aexp.ngbd.testDataflow;
+
+// Importing required Libraries
+import com.google.cloud.bigtable.beam.CloudBigtableIO;
+import com.google.cloud.bigtable.beam.CloudBigtableScanConfiguration;
+import com.google.cloud.bigtable.beam.CloudBigtableTableConfiguration;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult.State;
+import org.apache.beam.sdk.io.Read;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.hadoop.hbase.client.Result;
+
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.Default;
+
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubOptions;
+import org.apache.beam.sdk.options.StreamingOptions;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.TextIO;
+
+import org.joda.time.Duration;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
+import org.apache.beam.sdk.transforms.Create;
+
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.JSONObject;
+
+// Defining Class
+public class CsvBigtable {
+
+//	Defining Pipeline options
+	public interface BigtableOptions extends PipelineOptions {
+	    @Description("The Bigtable project ID, this can be different than your Dataflow project")
+	    @Default.String("lumi-local")
+	    String getBigtableProjectId();
+	
+	    void setBigtableProjectId(String bigtableProjectId);
+	
+	    @Description("The Bigtable instance ID")
+	    @Default.String("fake_instance")
+	    String getBigtableInstanceId();
+	
+	    void setBigtableInstanceId(String bigtableInstanceId);
+	
+	    @Description("The Bigtable table ID in the instance.")
+	    @Default.String("test_table")
+	    String getBigtableTableId();
+	
+	    void setBigtableTableId(String bigtableTableId);
+	    
+	    @Description("The Bigtable table Column Family.")
+	    @Default.String("cf1")
+	    String getBigtableColumnFamily();
+	
+	    void setBigtableColumnFamily(String bigtableColumnFamily);
+	    
+	    @Description("The Bigtable table Row Key.")
+	    @Default.String("phone#4c410523#20190501")
+	    String getBigtableRowKey();
+	
+	    void setBigtableRowKey(String bigtableRowKey);
+	    
+	    @Description("Input csv file")
+	    @Default.String("storage")
+	    String getInput();
+
+	    void setInput(String value);
+	    
+	    @Description("The Bigtable Columns list.")
+	    @Default.String("connected_cell,connected_wifi,os_build")
+	    String getBigtableColumns();
+	
+	    void setBigtableColumns(String bigtableColumns);
+	  }
+
+
+//	  Main Class
+	public static void main(String[] args) {
+		
+	    BigtableOptions options =
+	        PipelineOptionsFactory.fromArgs(args).withValidation().as(BigtableOptions.class);	    
+	    
+	    Pipeline pipeline  = Pipeline.create(options);	  
+	
+	    String ProjectId = options.getBigtableProjectId();
+	    String InstanceId = options.getBigtableInstanceId();
+	    String TableId = options.getBigtableTableId();
+	    String ColumnFamily = options.getBigtableColumnFamily();
+	    String RowKey = options.getBigtableRowKey();
+	    String InputFile = options.getInput();
+	    String TableColumns = options.getBigtableColumns();
+	    
+	    //Print input values for testing
+	    /*
+	    System.out.println(ProjectId);
+	    System.out.println(InstanceId);
+	    System.out.println(TableId);
+	    System.out.println(ColumnFamily);
+	    System.out.println(RowKey);
+	    System.out.println(InputFile);
+	    System.out.println(TableColumns);
+	    */
+	    	    
+	    CloudBigtableTableConfiguration config =
+	            new CloudBigtableTableConfiguration.Builder()
+	                .withProjectId(options.getBigtableProjectId())
+	                .withInstanceId(options.getBigtableInstanceId())
+	                .withTableId(options.getBigtableTableId())
+	                .build();
+	    
+	    System.out.println("Pipeline Execution Started");
+	    
+	    pipeline
+	    .apply("DefineCsvFileMatches", FileIO.match().filepattern(InputFile))
+        .apply("MatchCsvFiles", FileIO.readMatches())
+        .apply("ReadCsvFiles", TextIO.readFiles())
+	    .apply(
+		        ParDo.of(
+		            new DoFn<String, Mutation>() {
+		              @ProcessElement
+		              public void processElement(ProcessContext ctx, OutputReceiver<Mutation> out) {
+		      	        
+		            	//System.out.println("Input Row: " + ctx.element());	  
+		      	        String input = ctx.element();
+		      	        
+		      	        String[] input_rows = input.split(",");
+		      	        String[] input_columns = TableColumns.split(",");
+		      	        		      	       
+		      	        long timestamp = System.currentTimeMillis();
+		      	        Put row = new Put(Bytes.toBytes(RowKey));
+		      	        
+		      	        
+		      	        for (int i = 0; i<input_rows.length; i++) {
+		      	        	
+		      	        	//System.out.println("Input Row value: " + input_rows[i]);
+		      	        	
+		      	        	for(int j = 0; j<input_columns.length; j++){				      	    	
+				      	    	
+				      	    	if (i==j)
+				      	    	{
+				      	    		//System.out.println("Table Column Name: " + input_columns[j]);
+				      	    		row.addColumn(
+					                        Bytes.toBytes(ColumnFamily),
+					                        Bytes.toBytes(input_columns[j]),
+					                        timestamp,
+					                        Bytes.toBytes(input_rows[i]));
+				      	    	}				      	    	
+				      	    }
+		      	        }			                
+		                out.output(row);
+		      	      }
+		            }))
+	    
+	    .apply(CloudBigtableIO.writeToTable(config));
+	    
+	    System.out.println("Pipeline steps are executed");
+
+//		Running Pipeline to load data from CSV to Bigtable
+	    pipeline.run();
+	    
+	    System.out.println("Pipeline execution completed");
+        	   
+	  }
+}
---
+++ apachebeam/Gcs2Bigtable_dataflow (1).py
@@ -0,0 +1,25 @@
+# airflow
+from lumi.dag import DAG
+from lumi.dataFlowJavaOperator import DataFlowJavaOperator
+import os
+
+DATAFLOW_JOB = {"jar": "/opt/airflow/mount/testDataflow-bundled-0.0.1-SNAPSHOT.jar",  #gcp path of jar file 
+                "job_class": "com.aexp.ngbd.testDataflow.CsvBigtable", #Job Class
+                "job_name": "test-load-bigtable-via-lumisdk",  #dataflow job name
+                "pipeline_options": {
+                    'input': "/opt/airflow/mount/input.csv",  #gcp bucket path of input csv file
+                    'bigtableProjectId': "lumi-local",  #usecase project name 
+                    'bigtableInstanceId': "fake_instance",   #Bigtable instance ID 
+                    'bigtableTableId': "test_table",  #Bigtable name
+                    'bigtableColumnFamily': "cf1",  #Bigtable Column Family 
+                    'bigtableRowKey': "connected_cell#connected_wifi#os_build",  #Bigtable Row Keys 
+                    'bigtableColumns': "connected_cell,connected_wifi,os_build"  #Bigtable Column name(s)
+                },
+                }
+
+# define your dag
+with DAG(dag_id="dataflow-dag-lumisdk-Csv2Bigtable", #DAG Name 
+         schedule_interval=None
+         ) as dag:
+    submit_dataflow_job = DataFlowJavaOperator(task_id="submit_dataflow_job",
+                                               dataflow_job=DATAFLOW_JOB)
