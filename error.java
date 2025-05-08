886e3377784e8efbd43e776a3bc609232f72ea3e
vamsi9885-tech <nukalavamsikrishna06@gmail.com>
2025-05-08 16:13:35 +0530

    Create error.java

---
+++ error.java
@@ -0,0 +1,8 @@
+[INFO] -------------------------------------------------------------
+[ERROR] COMPILATION ERROR : 
+[INFO] -------------------------------------------------------------
+[ERROR] /C:/Users/kvchakr/Downloads/SparkBQToBT/SparkBQToBT/src/main/java/com/example/bqtobt/SparkBQToBT.java:[87,32] cannot find symbol
+  symbol:   method createColumnFamily(java.lang.String,java.lang.String)
+  location: variable adminClient of type com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient
+[INFO] 1 error
+[INFO] -------------------------------------------------------------
