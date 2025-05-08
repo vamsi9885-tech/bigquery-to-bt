15f6f7cc46ecf4be1980537ee0c9f254132c63a1
vamsi9885-tech <nukalavamsikrishna06@gmail.com>
2025-05-08 21:14:51 +0530

    Add files via upload

---
+++ dag_bal_otl2.py
@@ -0,0 +1,69 @@
+from lumi.dataprocCreateClusterOperator import DataprocCreateClusterOperator
+from lumi.dataprocSubmitJobOperator import DataprocSubmitJobOperator
+from lumi.dataprocDeleteClusterOperator import DataprocDeleteClusterOperator
+from lumi.dag import DAG
+from datetime import datetime, timedelta
+from google.cloud import bigtable
+   
+
+# parameters
+project_id = "axp-lumid"  # project id
+gear_project_id = config["gear_project_id"]  # gear project id
+bt_project_name = "prj-d-lumi-bt"  # lumi bigtable project
+cluster_name = "gear-bal-cluster-1"
+instance_id = "lumiplfeng-decppgusbt241030202635"  # Bigtable instance id
+bt_table_name = "gear_cars_logs_catalog_1"  # Bigtable table name
+dataset_id = "dw"  # Bigquery Dataset
+bq_table_name = "triumph_balance"  # Bigquery table name
+pyspark_uri = "gs://prj-d-gbl-gar-epgear/gear_hist_finapi/sample_testing/bq2bt_otl_testing/gear_balance/bal_otl2.py"
+json_file = "gear_hist_finapi/sample_testing/bq2bt_otl_mapping/bal_otl2_map.json"
+row_key = "cm13"
+
+# job object
+PYSPARK_JOB = {
+    "main_python_file_uri": pyspark_uri,
+    "jar_file_uris": ["gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar","gs://spark-lib/bigtable/spark-bigtable_2.12-0.4.0.jar"],
+    "args": [
+        f"--project_id={project_id}",
+        f"--gear_project_id={gear_project_id}",
+        f"--bt_project_name={bt_project_name}",
+        f"--instance_id={instance_id}",
+        f"--bt_table_name={bt_table_name}",
+        f"--bq_table_name={bq_table_name}",
+        f"--dataset_id={dataset_id}",
+        f"--json_file={json_file}",
+        f"--row_key={row_key}"
+    ]
+}
+spark_job = {
+    "refernce" : {"project_id" : "prj-d-gbl-gar-epgear"},
+    "placement":{"cluster_name" : "cluster_name"},
+    "spark_job":{
+        "main_class" : "com.example.bqtobt.SparkBQToBT",
+        "jar_file_uris" :["gs://prj-d-gbl-gar-epgear/gear_hist_finapi/sample_testing/testing_kvchakr/SparkBQToBT-bundled-1.0.jar"],
+        "args" :[""]
+    }
+
+}
+
+default_args = {
+    'owner': 'airflow',
+    "start_date": datetime.now(),
+    "retries": 1,
+    'retry_delay': timedelta(minutes=1)
+}
+# dag name
+DAG_ID = "gear_bal_bt_load_cars_catalog_testing"
+# dag
+with DAG(dag_id=DAG_ID, default_args=default_args, schedule_interval=None) as dag:
+    # Create Cluster
+    create_cluster = DataprocCreateClusterOperator(task_id="create_cluster", cluster_name=cluster_name,
+                                                   num_workers=2,
+                                                   idle_delete_ttl=1200)
+    # Execute the dataproc job
+    bigtable_bigquery = DataprocSubmitJobOperator(task_id="bq_to_bt", pyspark_job=PYSPARK_JOB, retries=0)
+    # Delete Cluster
+    delete_cluster = DataprocDeleteClusterOperator(task_id="delete_cluster", cluster_name=cluster_name)
+
+    # Order of Execution
+    create_cluster >> bigtable_bigquery >> delete_cluster
