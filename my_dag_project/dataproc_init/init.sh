2d84c0c05678ec08e8bd389354ca104958b1e48b
vamsi9885-tech <nukalavamsikrishna06@gmail.com>
2025-05-11 17:02:59 +0530

    Delete my_dag_project directory

--- my_dag_project/dataproc_init/init.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python3-pip
-pip3 install apache-beam[gcp] google-cloud-bigtable pyarrow
--- my_dag_project/spark_job/bq_to_gcs_parquet.py
+++
@@ -1,7 +0,0 @@
-
-from pyspark.sql import SparkSession
-import sys
-
-spark = SparkSession.builder.appName("BQ to GCS Parquet").getOrCreate()
-df = spark.read.format("bigquery").option("table", "your_dataset.your_table").load()
-df.write.mode("overwrite").parquet(sys.argv[1])
