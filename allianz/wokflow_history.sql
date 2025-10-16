7b72665fe6a9d3e2b81e8daf6048b145db090d35
vamsi9885-tech <nukalavamsikrishna06@gmail.com>
2025-10-16 09:15:36 +0530

    Update wokflow_history.sql

--- allianz/wokflow_history.sql
+++ allianz/wokflow_history.sql
@@ -1,187 +1,132 @@
-WITH rank_unique_TIMContactEvent AS (
-    SELECT * FROM (SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rnk,
-        aUniqueID,
-		aDateTime,
-		aHeader,
-        myAccident_clsno,
-		myAccident_instid,
-		myContactEventMethod_clsno,
-		myContactEventMethod_instid,
-		myContactEventReason_clsno,
-		myContactEventReason_instid,
-		myContactEventType_clsno,
-		myContactEventType_instid,
-		myContactEventStatus_clsno,
-		myContactEventStatus_instid,
-		myUser_clsno,
-		myUser_instid,
-		myContactToFrom_clsno,
-		myContactToFrom_instid
-    FROM dl_TIMContactEvent) t
-    WHERE t.rnk=1
-    AND t._operation <> 'D'
+WITH rank_unique_TIMWflProcess AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMWflProcess
+    FROM dl_TIMWflProcess
+),
+unique_TIMWflProcess AS (
+	SELECT *
+	FROM rank_unique_TIMWflProcess
+	WHERE rank_unique_TIMWflProcess = 1 AND _operation <> 'D'
 ),
 rank_unique_TIMPolicyClaim AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMPolicyClaim,
-        aClaimNumber,
-        myAccident_clsno,
-		myAccident_instid,
-		myPolicy_clsno,
-		myPolicy_instid
-    from dl_TIMPolicyClaim
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMPolicyClaim
+    FROM dl_TIMPolicyClaim
 ),
-rank_unique_TIMPolicy AS (
-    SELECT 
-		oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMPolicy,
-        aPolicyNumber
-    from dl_TIMPolicy
-),
-rank_unique_CTPAccident AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_CTPAccident,
-        aUniqueId
-    from dl_CTPAccident
-),
-rank_unique_TIMContactEventMethod AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactEventMethod,
-        aDescription
-    from dl_TIMContactEventMethod
-),
-rank_unique_TIMContactEventReason AS (
-    SELECT 
-		oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactEventReason,
-        aDescription
-    from dl_TIMContactEventReason
-),
-rank_unique_TIMContactEventType AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactEventType,
-        aDescription
-    from dl_TIMContactEventType
-),
-rank_unique_TIMContactEventStatus AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactEventStatus,
-        aDescription
-    from dl_TIMContactEventStatus
-),
-rank_unique_TIMContactIndividual AS (
-    SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactIndividual,
-        aUniqueID
-    from dl_TIMContactIndividual
+rank_unique_TIMWflActivity AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMWflActivity
+    FROM dl_TIMWflActivity
 ),
 rank_unique_TIMContactTypeUser AS (
     SELECT 
-    	oid_clsno,
-		oid_instid,
-		_timestamp,
-		_operation,
-        ROW_NUMBER() OVER (PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactTypeUser,
-        aUserID,
-		aKey
-    from dl_TIMContactTypeUser
+		aKey,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_TIMContactTypeUser,
+        _operation,
+        oid_clsno,
+        oid_instid
+    FROM dl_TIMContactTypeUser
+),
+rank_unique_CONWflForwardHistoryUpdate AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_CONWflForwardHistoryUpdate
+    FROM dl_CONWflForwardHistoryUpdate
+),
+rank_unique_CONWflReferralChangeReason AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_CONWflReferralChangeReason
+    FROM dl_CONWflReferralChangeReason
+),
+rank_unique_CONWflStateHistoryUpdate AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY  _timestamp DESC) AS rank_unique_CONWflStateHistoryUpdate
+    FROM dl_CONWflStateHistoryUpdate
+),
+rank_unique_CONWflProcessState AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_CONWflProcessState
+    FROM dl_CONWflProcessState
+),
+rank_unique_CONWflStateChangeReason AS (
+    SELECT *,
+        RANK() OVER(PARTITION BY oid_clsno, oid_instid ORDER BY _timestamp DESC) AS rank_unique_CONWflStateChangeReason
+    FROM dl_CONWflStateChangeReason
 )
 SELECT
-	rank_unique_TIMContactEvent.aUniqueID AS conev_id,
-	rank_unique_TIMPolicyClaim.aClaimNumber AS clm_no, 
-	rank_unique_CTPAccident.aUniqueId AS acc_no,
-	rank_unique_TIMPolicy.aPolicyNumber AS pol_no,
-	rank_unique_TIMContactTypeUser.aUserID AS user_id,
-	rank_unique_TIMContactEvent.aDateTime AS conev_acc_comm_dt,
-	NULL AS conev_acc_comm_frm, -- Refer to Confluence for detailed logic
-	NULL AS conev_acc_comms_to, -- Refer to Confluence for detailed logic 
-    rank_unique_TIMContactEvent.aHeader AS conev_acc_comm_subj,
-	NULL AS conev_acc_comm_doc_type, -- Refer to Confluence for detailed logic
-    rank_unique_TIMContactEventMethod.aDescription AS conev_acc_comm_meth,
-	rank_unique_TIMContactEventReason.aDescription AS conev_acc_comm_reason,
-	rank_unique_TIMContactEventStatus.aDescription AS conev_acc_comm_status,
-	rank_unique_TIMContactEventType.aDescription AS conev_acc_comm_type,
-	NULL AS conev_acc_comm_dist_not_verfd, -- TBC
-	NULL AS conev_acc_comm_frm_user_team -- TBC
-FROM rank_unique_TIMContactEvent
-INNER JOIN rank_unique_CTPAccident 
-	ON rank_unique_CTPAccident.oid_clsno = rank_unique_TIMContactEvent.myAccident_clsno
-	AND rank_unique_CTPAccident.oid_instid = rank_unique_TIMContactEvent.myAccident_instid
-	AND rank_unique_CTPAccident = 1
-	AND rank_unique_CTPAccident._operation <> 'D'
+	rank_unique_TIMWflActivity.aUniqueID AS wrkflw_no,
+	rank_unique_TIMPolicyClaim.aClaimNumber AS clm_no,
+	rank_unique_CONWflForwardHistoryUpdate.oid_instid AS hist_id,	
+	'Activity History' AS wrkflw_hist_type,
+	rank_unique_CONWflForwardHistoryUpdate._timestamp AS wrkflw_hist_time_dt,
+	user_forward_history.aKey AS wrkflw_hist_user,
+	rank_unique_CONWflForwardHistoryUpdate.aPrevReferStatus AS wrkflw_hist_frm_stat,
+	rank_unique_CONWflForwardHistoryUpdate.aNewReferStatus AS wrkflw_hist_to_stat,
+	rank_unique_CONWflReferralChangeReason.aName AS wrkflw_hist_rs
+FROM unique_TIMWflProcess
 INNER JOIN rank_unique_TIMPolicyClaim
-	ON rank_unique_TIMPolicyClaim.myAccident_clsno = rank_unique_CTPAccident.oid_clsno
-	AND rank_unique_TIMPolicyClaim.myAccident_instid = rank_unique_CTPAccident.oid_instid
+	ON rank_unique_TIMPolicyClaim.oid_clsno = unique_TIMWflProcess.myClaim_clsno
+	AND rank_unique_TIMPolicyClaim.oid_instid = unique_TIMWflProcess.myClaim_instid
 	AND rank_unique_TIMPolicyClaim = 1
 	AND rank_unique_TIMPolicyClaim._operation <> 'D'
-LEFT JOIN rank_unique_TIMPolicy
-	ON rank_unique_TIMPolicy.oid_clsno = rank_unique_TIMPolicyClaim.myPolicy_clsno
-	AND rank_unique_TIMPolicy.oid_instid = rank_unique_TIMPolicyClaim.myPolicy_instid 
-	AND rank_unique_TIMPolicy = 1
-	AND rank_unique_TIMPolicy._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactEventMethod
-	ON rank_unique_TIMContactEventMethod.oid_clsno = rank_unique_TIMContactEvent.myContactEventMethod_clsno 
-	AND rank_unique_TIMContactEventMethod.oid_instid = rank_unique_TIMContactEvent.myContactEventMethod_instid 
-	AND rank_unique_TIMContactEventMethod = 1
-	AND rank_unique_TIMContactEventMethod._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactEventReason
-	ON rank_unique_TIMContactEventReason.oid_clsno = rank_unique_TIMContactEvent.myContactEventReason_clsno 
-	AND rank_unique_TIMContactEventReason.oid_instid = rank_unique_TIMContactEvent.myContactEventReason_instid 
-	AND rank_unique_TIMContactEventReason = 1
-	AND rank_unique_TIMContactEventReason._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactEventType
-	ON rank_unique_TIMContactEventType.oid_clsno = rank_unique_TIMContactEvent.myContactEventType_clsno 
-	AND rank_unique_TIMContactEventType.oid_instid = rank_unique_TIMContactEvent.myContactEventType_instid 
-	AND rank_unique_TIMContactEventType = 1
-	AND rank_unique_TIMContactEventType._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactEventStatus
-	ON rank_unique_TIMContactEventStatus.oid_clsno = rank_unique_TIMContactEvent.myContactEventStatus_clsno 
-	AND rank_unique_TIMContactEventStatus.oid_instid = rank_unique_TIMContactEvent.myContactEventStatus_instid 
-	AND rank_unique_TIMContactEventStatus = 1
-	AND rank_unique_TIMContactEventStatus._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactTypeUser
-	ON rank_unique_TIMContactTypeUser.oid_clsno = rank_unique_TIMContactEvent.myUser_clsno 
-	AND rank_unique_TIMContactTypeUser.oid_instid = rank_unique_TIMContactEvent.myUser_instid 
-	AND rank_unique_TIMContactTypeUser = 1
-	AND rank_unique_TIMContactTypeUser._operation <> 'D'
-LEFT JOIN rank_unique_TIMContactIndividual
-	ON rank_unique_TIMContactIndividual.oid_clsno = rank_unique_TIMContactEvent.myContactToFrom_clsno 
-	AND rank_unique_TIMContactIndividual.oid_instid = rank_unique_TIMContactEvent.myContactToFrom_instid 
-	AND rank_unique_TIMContactIndividual = 1
-	AND rank_unique_TIMContactIndividual._operation <> 'D'
-
+INNER JOIN  rank_unique_TIMWflActivity
+	ON  rank_unique_TIMWflActivity.myWflProcess_clsno = unique_TIMWflProcess.oid_clsno
+	AND rank_unique_TIMWflActivity.myWflProcess_instid = unique_TIMWflProcess.oid_instid
+	AND rank_unique_TIMWflActivity = 1
+	AND rank_unique_TIMWflActivity._operation <> 'D'
+INNER JOIN  rank_unique_CONWflForwardHistoryUpdate
+	ON  rank_unique_CONWflForwardHistoryUpdate.myHistoriedComponent_clsno = rank_unique_TIMWflActivity.oid_clsno
+	AND rank_unique_CONWflForwardHistoryUpdate.myHistoriedComponent_instid = rank_unique_TIMWflActivity.oid_instid
+	AND rank_unique_CONWflForwardHistoryUpdate = 1
+	AND rank_unique_CONWflForwardHistoryUpdate._operation <> 'D'
+LEFT JOIN  rank_unique_TIMContactTypeUser AS user_forward_history
+	ON  user_forward_history.oid_clsno = rank_unique_CONWflForwardHistoryUpdate.myCreatedBy_clsno
+	AND user_forward_history.oid_instid = rank_unique_CONWflForwardHistoryUpdate.myCreatedBy_instid
+	AND user_forward_history.rank_unique_TIMContactTypeUser = 1
+	AND user_forward_history._operation <> 'D'
+LEFT JOIN  rank_unique_CONWflReferralChangeReason
+	ON  rank_unique_CONWflReferralChangeReason.oid_clsno = rank_unique_CONWflForwardHistoryUpdate.myChangeReason_clsno
+	AND rank_unique_CONWflReferralChangeReason.oid_instid = rank_unique_CONWflForwardHistoryUpdate.myChangeReason_instid
+	AND rank_unique_CONWflReferralChangeReason = 1
+	AND rank_unique_CONWflReferralChangeReason._operation <> 'D'
+UNION ALL
+SELECT
+	unique_TIMWflProcess.aUniqueID AS wrkflw_no,
+	rank_unique_TIMPolicyClaim.aClaimNumber AS clm_no,
+	rank_unique_CONWflStateHistoryUpdate.oid_instid AS hist_id,
+	'Process History' AS wrkflw_hist_type,
+	rank_unique_CONWflStateHistoryUpdate.aLastModified AS wrkflw_hist_time_dt,
+	user_state_history.aKey AS wrkflw_hist_user,
+	ProcessState_from.aName AS wrkflw_hist_frm_stat,
+	ProcessState_to.aName AS wrkflw_hist_to_stat,
+	rank_unique_CONWflStateChangeReason.aName AS wrkflw_hist_rs
+FROM unique_TIMWflProcess
+INNER JOIN rank_unique_TIMPolicyClaim
+	ON rank_unique_TIMPolicyClaim.oid_clsno = unique_TIMWflProcess.myClaim_clsno
+	AND rank_unique_TIMPolicyClaim.oid_instid = unique_TIMWflProcess.myClaim_instid
+	AND rank_unique_TIMPolicyClaim = 1
+	AND rank_unique_TIMPolicyClaim._operation <> 'D'
+INNER JOIN  rank_unique_CONWflStateHistoryUpdate
+	ON  rank_unique_CONWflStateHistoryUpdate.myHistoriedComponent_clsno = unique_TIMWflProcess.oid_clsno
+	AND rank_unique_CONWflStateHistoryUpdate.myHistoriedComponent_instid = unique_TIMWflProcess.oid_instid
+	AND rank_unique_CONWflStateHistoryUpdate = 1
+	AND rank_unique_CONWflStateHistoryUpdate._operation <> 'D'
+LEFT JOIN  rank_unique_TIMContactTypeUser AS user_state_history
+	ON  user_state_history.oid_clsno = rank_unique_CONWflStateHistoryUpdate.myCreatedBy_clsno
+	AND user_state_history.oid_instid = rank_unique_CONWflStateHistoryUpdate.myCreatedBy_instid
+	AND user_state_history.rank_unique_TIMContactTypeUser = 1
+	AND user_state_history._operation <> 'D'
+LEFT JOIN  rank_unique_CONWflProcessState AS ProcessState_from
+	ON  ProcessState_from.oid_clsno = rank_unique_CONWflStateHistoryUpdate.myPreviousState_clsno
+	AND ProcessState_from.oid_instid = rank_unique_CONWflStateHistoryUpdate.myPreviousState_instid
+	AND ProcessState_from.rank_unique_CONWflProcessState = 1
+	AND ProcessState_from._operation <> 'D'
+LEFT JOIN  rank_unique_CONWflProcessState AS ProcessState_to
+	ON  ProcessState_to.oid_clsno = rank_unique_CONWflStateHistoryUpdate.myNewState_clsno
+	AND ProcessState_to.oid_instid = rank_unique_CONWflStateHistoryUpdate.myNewState_instid
+	AND ProcessState_to.rank_unique_CONWflProcessState = 1
+	AND ProcessState_to._operation <> 'D'
+LEFT JOIN  rank_unique_CONWflStateChangeReason
+	ON  rank_unique_CONWflStateChangeReason.oid_clsno = rank_unique_CONWflStateHistoryUpdate.myChangeReason_clsno
+	AND rank_unique_CONWflStateChangeReason.oid_instid = rank_unique_CONWflStateHistoryUpdate.myChangeReason_instid
+	AND rank_unique_CONWflStateChangeReason = 1
+	AND rank_unique_CONWflStateChangeReason._operation <> 'D'
